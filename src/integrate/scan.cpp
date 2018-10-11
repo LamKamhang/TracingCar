bee6e504eef8efb3b174dc9c0a49a39c66db37dd
LamKamhang <cool_lam@outlook.com>
2018-10-11 17:13:28 +0800

    增加扫码模块

---
+++ src/integrate/scan.cpp
@@ -0,0 +1,95 @@
+#include "scan.h"
+
+ScanModule::ScanModule()
+    : mySerial(_SOFT_RX_PIN_, _SOFT_TX_PIN_)
+    , flag_SetSeiDecMod(true)
+    , flag_StaDec(false)
+{
+}
+
+void ScanModule::init()
+{
+    mySerial.begin(9600);
+}
+
+char ScanModule::run()
+{
+    delay(300);
+    //Serial.println("scaning...");
+   char get_char = ' ';
+   String SciString = "";
+   int Position = 0;
+   
+   //Serial.println(flag_SetSeiDecMod);
+   if(flag_SetSeiDecMod == true)       //模块初始化功能
+   {
+      for(int i = 0;i < sizeof(SetSeiDecMod);i++)
+      {
+        mySerial.write(SetSeiDecMod[i]);
+        delay(1);
+      }
+      flag_SetSeiDecMod = false;
+      flag_StaDec = true;
+   }
+
+  //Serial.println(flag_StaDec);
+   if(flag_StaDec == true)     //模块扫码功能
+   {
+      for(int i = 0;i < sizeof(StaDec);i++)
+      {
+         mySerial.write(StaDec[i]);
+         delay(1);
+       }
+    }
+   delay(10);    
+   while(mySerial.available())    //所扫的字符串返回
+   {
+        char inChar = (char)mySerial.read();
+        delay(5);
+        SciString += inChar;
+   }
+   
+   Position = SciString.indexOf('Z');  //找到字符Z的位置  
+
+  //Serial.println(Position);
+   if(Position != -1)   //字符'Z'找到
+   {
+      SciString = SciString.substring(Position,SciString.length());  //截取有效字符串 e.g    sdaaf,ZJGXDS01 --------->  ZJGXDS01
+                                                                     //                        返回的字符串        截取到有效的字符串
+      get_char = SciString.charAt(7);  // 返回第7位字符  e.g   ZJGXDS01  ---->    1
+   }
+   else   //字符'Z'未能找到   
+   {
+      flag_SetSeiDecMod = true;  //初始化标志位  方便第二次扫码
+      flag_StaDec   = false;
+    } 
+    return get_char;
+}
+
+char ScanModule::scan()
+{
+	char res = 0;
+	char flag;
+	unsigned long time = millis();
+	while (millis() - time < 1000)
+	{
+		flag = run();
+		if (flag >= '0' && flag <= '9')
+		{
+			res = check_target(flag);
+			break;
+		}
+	}
+	return res;
+}
+
+char ScanModule::check_target(char scan_char)
+{
+    for (char id = 0; id < num_target; ++id)
+    {
+        if (target[id] == scan_char)
+            return id+1;
+    }
+    return 0;
+}
+
---
+++ src/integrate/scan.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+// gnd, vcc, SOFT_TX_PIN, SOFT_RX_PIN
+#define _SOFT_RX_PIN_   50
+#define _SOFT_TX_PIN_   51
+
+const unsigned char SetSeiDecMod[] = {0x07,0xC6,0x04,0x00,0xFF,0x8A,0x08,0xFD,0x9E};  //扫码模块初始化协议
+const unsigned char StaDec[] = {0x04,0xE4,0x04,0x00,0xFF,0x14};   //扫码模块扫码协议
+
+const char target[] = { '6','9','0' };
+const char num_target = sizeof(target) / sizeof(target[0]);
+
+class ScanModule
+{
+public:
+    ScanModule();
+    void init();
+	char scan();
+private:
+    SoftwareSerial mySerial;
+    boolean flag_SetSeiDecMod;
+    boolean flag_StaDec;
+	char run();
+	char check_target(char scan_char);
+};
